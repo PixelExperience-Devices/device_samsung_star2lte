1236ca5cebd62819d30ccc0ce73e7dc4ef196aac
Dylan Neve <dylanneve1@gmail.com>
2022-05-07 17:15:28 +0100

    star2lte: Define TARGET_BOOT_ANIMATION_RES to 1440p

--- aosp_star2lte.mk
+++ aosp_star2lte.mk
@@ -26,6 +26,7 @@ $(call inherit-product, device/samsung/star2lte/device.mk)
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOT_ANIMATION_RES := 1440
 
 ## Inherit some common AOSP stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
